118d637c36d98c5faa0202115ad474dfb2417090
heedahee <106054124+heedahee@users.noreply.github.com>
2022-06-07 23:52:03 +0900

    모바일

--- app/src/main/java/com/example/mydietdiary/MainActivity.java
+++ app/src/main/java/com/example/mydietdiary/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.mydietdiary;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
-    DatePicker dp;
+    CalendarView calendarView;
+    TextView today;
     Button btn1, btn2, btn3;
     EditText et;
     String filename;
@@ -21,11 +29,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("MDD");
-        dp = findViewById(R.id.dp);
+        calendarView = findViewById(R.id.calendarView);
+        today = findViewById(R.id.today);
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
         btn3 = findViewById(R.id.btn3);
         et = findViewById(R.id.et);
+
+        DateFormat formatter = new SimpleDateFormat("yyyy년MM월dd일");
+        Date date = new Date(calendarView.getDate());
+        today.setText(formatter.format(date));
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                @Override
+                        public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth){
+                    String day;
+                    day = year + "년" + (month+1) + "월" + dayOfMonth + "일";
+                    today.setText(day);
+                }
+        });
+
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,9 +70,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent3);
             }
         });
-        Calendar sss = Calendar.getInstance();
-        int cy = sss.get(Calendar.YEAR);
-        int cm = sss.get(Calendar.MONTH);
-        int cd = sss.get(Calendar.DAY_OF_MONTH);
+
     }
 }
