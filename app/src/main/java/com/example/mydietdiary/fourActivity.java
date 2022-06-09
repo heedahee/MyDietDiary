510cb0e4170ed3d57afc831aa7a7cadef6695812
heedahee <106054124+heedahee@users.noreply.github.com>
2022-06-09 09:18:36 +0900

    모바일

--- app/src/main/java/com/example/mydietdiary/fourActivity.java
+++ app/src/main/java/com/example/mydietdiary/fourActivity.java
@@ -20,18 +20,17 @@ import java.util.ArrayList;
 
 public class fourActivity extends AppCompatActivity {
 
-    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
     ListView listViewMP3;
 
-    String mp3Path = Environment.getExternalStorageDirectory().getPath()+"/";
+    String mp3Path = Environment.getExternalStorageDirectory().getPath() + "/";
     MediaPlayer mplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.four);
-        ActivityCompat.requestDragAndDropPermissions(this, new String[]{android.
-                Manifest.permission.WRITE_EXTERNAL_STORAGE}.MODE_PRIVATE);
+        ActivityCompat.requestDragAndDropPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}.MODE_PRIVATE);
 
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
@@ -39,9 +38,6 @@ public class fourActivity extends AppCompatActivity {
         btn4 = findViewById(R.id.btn4);
         btn5 = findViewById(R.id.btn5);
         btn6 = findViewById(R.id.btn6);
-        btn7 = findViewById(R.id.btn7);
-        btn8 = findViewById(R.id.btn8);
-        btn9 = findViewById(R.id.btn9);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,33 +81,12 @@ public class fourActivity extends AppCompatActivity {
                 finish();
             }
         });
-        btn7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    mplayer = new MediaPlayer();
-                    mplayer.setDataSource(mp3Path + selectMP3);
-                    mplayer.prepare();
-                    mplayer.start();
-                    btn7.setClickable(false);
-                    btn8.setClickable(true);
-                } catch (IOException e) {
 
-                }
-            }
-        });
-        btn9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-    }
-        final String[]mid = {"LOVE DIVE - 아이브","INVU - 태연", "MY BAG - 아이들",
-                "VOYAGER - 기현", "TOMBOY - 아이들", "DM - 프로미스나인", "SMILEY - 예나"};
+        final String[] mid = {"LOVE me - 비오", "Ballerino - 리쌍", "먹색의 별 - 디핵",
+                "비누 - 비비", "인생네컷 - Gist", "꽃다운 나이 - 릴러말즈", "단추 - 릴러말즈"};
         ListView list = (ListView) findViewById(R.id.listview1);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,mid);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mid);
         list.setAdapter(adapter);
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -121,34 +96,23 @@ public class fourActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             }
 
-            final String[]mid = {"Dance Monkey - Tones And I","Every second - Mina Okabe", "Shut up - Mia Rodriguez",
-                    "Working - Tate McRae,Khalid", "Hold on - Justin Bieber", "Hey siri - salem ilese"};
-            ListView list = (ListView) findViewById(R.id.listview2);
+        });
+        final String[]mid2 = {"Dance Monkey - Tones And I","Every second - Mina Okabe", "Shut up - Mia Rodriguez",
+                "Working - Tate McRae,Khalid", "Hold on - Justin Bieber", "Hey siri - salem ilese"};
+        ListView list2 = (ListView) findViewById(R.id.listview2);
 
-                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2,mid);
-                 list.setAdapter(adapter);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, mid2);
+        list2.setAdapter(adapter2);
 
-                 list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                     @Override
-                    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                        Toast.makeText(getApplicationContext(), mid[arg2],
-                                 Toast.LENGTH_SHORT).show();
-                     }
-
-                final String[]mid = {"LOVE me - 비오","Ballerino - 리쌍", "먹색의 별 - 디핵",
-                        "비누 - 비비", "인생네컷 - Gist", "꽃다운 나이 - 릴러말즈", "단추 - 릴러말즈"};
-                ListView list = (ListView) findViewById(R.id.listview3);
-
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,mid);
-                list.setAdapter(adapter);
-
-                list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                        Toast.makeText(getApplicationContext(), mid[arg2],
-                                Toast.LENGTH_SHORT).show();
-                    }
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                Toast.makeText(getApplicationContext(), mid2[arg2],
+                        Toast.LENGTH_SHORT).show();
+            }
 
         });
     }
+
+}
 
